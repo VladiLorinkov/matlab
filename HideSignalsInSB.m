e7e9bfd223b94dfc89bd2d580641ab8de2ab3499
Vladimir Lorinkov <39917190+VladiLorinkov@users.noreply.github.com>
2020-08-05 11:26:58 +0300

    Update HideSignalsInSB.m
    Looping trough each test case,
    in which all shown signals are allocated.
    The inputs for leading and/or trailing strings from the user are matched with the allocated signals.
    
    TODO --> index those signals and hide them from the signal builder

--- HideSignalsInSB.m
+++ HideSignalsInSB.m
@@ -1,7 +1,8 @@
 %% This script hides all signals in all test cases in a signal builder with a specified leading or trailing string
 %It is usable only on Matlab-Simulink model and is convenient for Signal
-%Handling models that are using Signal Builder as testing environment
-
+%handling models that are using Signal Builder as testing environment.
+%Developed in Matlab 2016b and not sure if it is compatible with other
+%versions
 
 %finds all Signal builder blocks in the model and list them in array
 findAllSBs = find_system(bdroot, 'StartFcn','sigbuilder_block(''start'');');
@@ -22,7 +23,6 @@ inStr = inputdlg(inputString,'Collect data',[1 35]);
 if(isempty(inStr{1}) && isempty(inStr{2}))
     errordlg('No data input was specified');
     error('no data input was specified');
-
     
 % If both leading and trailing strings were specified make sure it is
 % not human error
@@ -30,15 +30,65 @@ elseif(~isempty(inStr{1}) && ~isempty(inStr{2}))
         quest = {'OK', 'Cancel'};
         answer = questdlg('Both leading and trailing strings were found, are you sure?','Warning');
 
-        if(~strcmp(answer, 'YES'))
+        if(~strcmp(answer, 'Yes'))
                 errordlg('Script terminated');
                 error('Terminated by user');
         end   
 end
 
-%opening of the selected signal builder and getting the data from it
+searchLead = (~isempty(inStr{1}));
+searchTrail = (~isempty(inStr{2}));
+processGroups = waitbar(0, 'Loading data');
+
+%opening of the selected signal builder and extracting the data from it
 open_system(findAllSBs(selectedSB), 'OpenFcn')
 set(get_param(cell2mat(findAllSBs(selectedSB)), 'Userdata'), 'visible', 'off');
 [time, data, signals, testGroups] = signalbuilder(cell2mat(findAllSBs(selectedSB)));
+    
+%local signal builder data to extract the shown signals from a test case
+sbData = get(get_param(cell2mat(findAllSBs(selectedSB)), 'UserData'), 'UserData');
 numGroups = size(testGroups, 2);
+
+%expressions for matching the leading and the trailing strings of the shown
+%signals
+leadStrExpr = strcat(inStr{1}, '.*');
+trailStrExpr = strcat('.*', inStr{2});
+
+
+%looping trough all test cases of the signal builder
+for iGroup = 1:numGroups
+    waitbar(iGroup/numGroups,processGroups, sprintf('%d / %d', iGroup, numGroups))
+    
+    %switching the test case
+    signalbuilder(cell2mat(findAllSBs(selectedSB)), 'activegroup', iGroup);
+    
+    %getting the shown signals in the test case
+    activeSigIdx = sbData.dataSet(iGroup).activeDispIdx; 
+    shownSignalNames = signals(activeSigIdx);
+    matchLead = [];
+    matchTrail = [];
+    signalsToHide = '';
+    
+    
+    if (searchLead)
+        matchLead = regexp(shownSignalNames, leadStrExpr);
+    end
+
+    if (searchTrail)
+        matchTrail = regexp(shownSignalNames, trailStrExpr);
+    end
+    
+    
+%signalsToHide = shownSignalNames{matchedSignalIndx};
+    
+    shownSignals = size(shownSignalNames, 2);
+    %looping trough the shown signals
+    for iSignal = 1:shownSignals
+        %TODO
+    end
+
+%signalbuilder(cell2mat(findAllSBs(selectedSB)), 'activegroup', iGroup);
+end
+
+close(processGroups)
 
